9f2e40f75245be69a8acda66d50e0c4791401b0f
Arun Tajpuriya <aruntajpuriya1@gmail.com>
2024-07-13 16:49:45 +0545

    first commit

---
+++ app/(tabs)/explore.jsx
@@ -0,0 +1,15 @@
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const explore = () => {
+  return (
+    <View>
+      <Text>More</Text>
+    </View>
+  )
+}
+
+export default explore
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const home = () => {
+  return (
+    <View style={{
+        
+
+        
+      }}>
+      
+      <Text style={{
+        fontSize:40
+      }}>Home</Text>
+    </View>
+  )
+}
+
+export default home
+
+const styles = StyleSheet.create({})
